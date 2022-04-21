2937ce9bec7284326ae84fcfb935e7ecd762f041
Adison Green <adison@Adisons-MacBook-Pro.local>
2022-04-21 10:05:22 -0600

    Color Extenison

--- PokedexApp/API/StorePokemon.swift
+++ PokedexApp/API/StorePokemon.swift
@@ -67,25 +67,25 @@ enum PokemonType: String, Decodable {
     
     var color: UIColor {
         switch self {
-            case .grass: return UIColor(cgColor: CGColor(red: 0.3, green: 0.9, blue: 0.3, alpha: 1.0))
-            case .poison: return UIColor(cgColor: CGColor(red: 0.5, green: 0.1, blue: 0.5, alpha: 1.0))
-            case .fire: return UIColor(cgColor: CGColor(red: 0.9, green: 0.2, blue: 0.2, alpha: 1.0))
-            case .flying: return UIColor(cgColor: CGColor(red: 0.1, green: 0.7, blue: 0.8, alpha: 1.0))
-            case .bug: return UIColor(cgColor: CGColor(red: 0.2, green: 0.4, blue: 0.1, alpha: 1.0))
-            case .normal: return UIColor(cgColor: CGColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0))
-            case .electric: return UIColor(cgColor: CGColor(red: 0.9, green: 0.9, blue: 0.1, alpha: 1.0))
-            case .rock: return UIColor(cgColor: CGColor(red: 0.5, green: 0.4, blue: 0.2, alpha: 1.0))
-            case .ground: return UIColor(cgColor: CGColor(red: 0.7, green: 0.5, blue: 0.1, alpha: 1.0))
-            case .water: return UIColor(cgColor: CGColor(red: 0.1, green: 0.5, blue: 0.9, alpha: 1.0))
-            case .fairy: return UIColor(cgColor: CGColor(red: 0.9, green: 0.5, blue: 0.6, alpha: 1.0))
-            case .fighting: return UIColor(cgColor: CGColor(red: 0.9, green: 0.4, blue: 0.1, alpha: 1.0))
-            case .psychic: return UIColor(cgColor: CGColor(red: 0.9, green: 0.3, blue: 0.4, alpha: 1.0))
-            case .ice: return UIColor(cgColor: CGColor(red: 0.3, green: 0.7, blue: 0.9, alpha: 1.0))
-            case .dragon: return UIColor(cgColor: CGColor(red: 0.3, green: 0.3, blue: 0.9, alpha: 1.0))
-            case .steel: return UIColor(cgColor: CGColor(red: 0.8, green: 0.8, blue: 0.9, alpha: 1.0))
-            case .ghost: return UIColor(cgColor: CGColor(red: 0.6, green: 0.4, blue: 0.6, alpha: 1.0))
-            case .dark: return UIColor(cgColor: CGColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0))
-            case .unknown: return UIColor.gray
+        case .grass: return .grassGreen
+        case .poison: return .poisonPurple
+        case .fire: return .fireRed
+        case .flying: return .flyingBlue
+        case .bug: return .bugGreen
+        case .normal: return .normalGray
+        case .electric: return .electricYellow
+        case .rock: return .rockBrown
+        case .ground: return .groundBrown
+        case .water: return .waterBlue
+        case .fairy: return .fairyPink
+        case .fighting: return .fightingOrange
+        case .psychic: return .psychicPink
+        case .ice: return .iceBlue
+        case .dragon: return .dragonBlue
+        case .steel: return .steelGray
+        case .ghost: return .ghostPurple
+        case .dark: return .darkBlack
+        case .unknown: return .gray
         }
     }
 }
---
+++ PokedexApp/Other/ColorExtension.swift
@@ -0,0 +1,30 @@
+//
+//  ColorExtension.swift
+//  PokedexApp
+//
+//  Created by Adison Green on 4/21/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let grassGreen = UIColor(cgColor: CGColor(red: 0.3, green: 0.9, blue: 0.3, alpha: 1.0))
+    static let poisonPurple = UIColor(cgColor: CGColor(red: 0.5, green: 0.1, blue: 0.5, alpha: 1.0))
+    static let fireRed = UIColor(cgColor: CGColor(red: 0.9, green: 0.2, blue: 0.2, alpha: 1.0))
+    static let flyingBlue = UIColor(cgColor: CGColor(red: 0.1, green: 0.7, blue: 0.8, alpha: 1.0))
+    static let bugGreen = UIColor(cgColor: CGColor(red: 0.2, green: 0.4, blue: 0.1, alpha: 1.0))
+    static let normalGray = UIColor(cgColor: CGColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0))
+    static let electricYellow = UIColor(cgColor: CGColor(red: 0.9, green: 0.9, blue: 0.1, alpha: 1.0))
+    static let rockBrown = UIColor(cgColor: CGColor(red: 0.5, green: 0.4, blue: 0.2, alpha: 1.0))
+    static let groundBrown = UIColor(cgColor: CGColor(red: 0.7, green: 0.5, blue: 0.1, alpha: 1.0))
+    static let waterBlue = UIColor(cgColor: CGColor(red: 0.1, green: 0.5, blue: 0.9, alpha: 1.0))
+    static let fairyPink = UIColor(cgColor: CGColor(red: 0.9, green: 0.5, blue: 0.6, alpha: 1.0))
+    static let fightingOrange = UIColor(cgColor: CGColor(red: 0.9, green: 0.4, blue: 0.1, alpha: 1.0))
+    static let psychicPink = UIColor(cgColor: CGColor(red: 0.9, green: 0.3, blue: 0.4, alpha: 1.0))
+    static let iceBlue = UIColor(cgColor: CGColor(red: 0.3, green: 0.7, blue: 0.9, alpha: 1.0))
+    static let dragonBlue = UIColor(cgColor: CGColor(red: 0.3, green: 0.3, blue: 0.9, alpha: 1.0))
+    static let steelGray = UIColor(cgColor: CGColor(red: 0.8, green: 0.8, blue: 0.9, alpha: 1.0))
+    static let ghostPurple = UIColor(cgColor: CGColor(red: 0.6, green: 0.4, blue: 0.6, alpha: 1.0))
+    static let darkBlack = UIColor(cgColor: CGColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0))
+}
